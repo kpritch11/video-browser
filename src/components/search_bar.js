a75262a8d32919cc16a89d351637b6611b6ce060
kurtispritchard <kpritch11@gmail.com>
2016-07-30 11:14:12 -0500

    Refactored search_bar from functional component to class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,12 @@
 // Import the code we need
-import React from 'react';
+import React, { Component } from 'react'; // get React and React.Component, now React.Component can be accessed shorthand with Component
 
-// Create a React component
-const SearchBar = () => {
-    return <input />;
-};
+// Create a React component class
+class SearchBar extends Component {
+    render() {
+        return <input />;
+    }
+}
 
 // Export the React component
 export default SearchBar;
