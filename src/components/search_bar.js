3a5ff8ba58fc6a50beb1b1453c7f653d6e09d876
kurtispritchard <kpritch11@gmail.com>
2016-07-30 12:03:02 -0500

    Setup search bar constructor and term state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react'; // get React and React.Component, now 
 
 // Create a React component class
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { term: '' };
+    }
     render() {
         return <input onChange={event => console.log(event.target.value)} />;
     }
