c85e83c48e21458c2f9e4751ac50b6c6e59ff046
kurtispritchard <kpritch11@gmail.com>
2016-07-31 11:26:56 -0500

    Rendered VideoDetail.

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const VideoDetail = ({video}) => {
+    if (!video) {
+        return <div>Loading...</div>;
+    }
+    
     const videoID = video.id.videoId;
-    const url = `https://www.youtube.com/embed/${videoID}` // ES6 for 'https://www.youtube.com/embed/' + videoID // note ES6 string interpolation uses `` and regular uses ''
+    const url = `https://www.youtube.com/embed/${videoID}`; // ES6 for 'https://www.youtube.com/embed/' + videoID // note ES6 string interpolation uses `` and regular uses ''
     return (
         <div className="video-detail col-md-8">
             <div className="embed-responsive embed-responsive-16by9">
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAE782XusBin97nbDNrrw28Hdd-hn0BRp8';
 
 // Create a React component
@@ -20,6 +21,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
