8bf4b639f644a61a9636d79b838617ef535c2ae9
kurtispritchard <kpritch11@gmail.com>
2016-07-31 09:46:57 -0500

    Created VideoList function component. Passed videos into VideoList and printed lengh of array.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAE782XusBin97nbDNrrw28Hdd-hn0BRp8';
 
 // Create a React component
@@ -19,6 +20,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
