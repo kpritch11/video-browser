ba25569cd6b549d49e66d31a6f442ac3a11910c8
kurtispritchard <kpritch11@gmail.com>
2016-07-31 09:34:48 -0500

    Updated App to class component. Moved YTSearch into constructor. Set state with videos data response.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 // Import the code we need
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
-import SearchBar from './components/search_bar'; // ./ means current directory
-
+import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyAE782XusBin97nbDNrrw28Hdd-hn0BRp8';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
+// Create a React component
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
 
-// Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos }); // same as this.setState({ videos: videos}); // this.state.videos: videos (passed in from function and set to state)
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container')); // <App /> is shorthand for <App></App> (must be empty component to use shorthand)
+ReactDOM.render(<App />, document.querySelector('.container'));
